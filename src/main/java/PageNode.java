b33a9195c73b8bf9403bb0feb65a81aebfdb35c0
kelsiec <kelsiec@maizeanalytics.com>
2019-03-03 23:02:49 -0800

    fix pagenode

--- src/main/java/PageNode.java
+++ src/main/java/PageNode.java
@@ -1,10 +1,9 @@
 import java.util.Set;
-import java.util.TreeSet;
 
 public class PageNode {
     private int pageNumber;
     private String text;
     private boolean isEnding;
-    private Set<PageNode> children = new TreeSet<>();
+    private Set<PageNode> children;
 
 }
